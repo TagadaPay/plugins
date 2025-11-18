8d8710bd41536c233d68875772c7edec8e959361
Etienne Guillet <guilletetienne77@gmail.com>
2025-11-18 19:59:10 +0900

    feat: update bogo to latest version of sdk

--- bogo/src/components/Providers.tsx
+++ bogo/src/components/Providers.tsx
@@ -1,35 +1,9 @@
-import {
-  createRawPluginConfig,
-  RawPluginConfig,
-  TagadaProvider,
-} from "@tagadapay/plugin-sdk/v2";
-import { useEffect, useState } from "react";
+import { TagadaProvider } from "@tagadapay/plugin-sdk/v2";
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
-  const [rawPluginConfig, setRawPluginConfig] = useState<
-    RawPluginConfig | undefined
-  >(undefined);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    createRawPluginConfig().then((config) => {
-      setRawPluginConfig(config);
-      console.log("rawPluginConfig", config);
-      setIsLoading(false);
-    });
-  }, []);
-
-  if (isLoading) {
-    return null;
-  }
-
-  return (
-    <TagadaProvider environment="development" rawPluginConfig={rawPluginConfig}>
-      {children}
-    </TagadaProvider>
-  );
+  return <TagadaProvider>{children}</TagadaProvider>;
 }
